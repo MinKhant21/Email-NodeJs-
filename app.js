2d5e6ba868b96e08ea2faa1b6e8f653847d13230
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-09-26 15:00:47 +0630

    DoneEmail

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import epxress from 'express';
 import { router } from './router';
+import {engine} from 'express-edge';
+import  BodyParser  from 'body-parser';
+
 
 const app = epxress();
+
+//view engine
+app.use(engine);
+app.set('views',`${__dirname}/page`);
+//static file
+app.use(epxress.static('public'));
+
+//body parser
+app.use(BodyParser.urlencoded({ extended: false }))
+app.use(BodyParser.json())
+
 app.use(router);
 app.listen(3000,()=>console.log('Server Running ......'))
--- router.js
+++ router.js
@@ -1,6 +1,53 @@
 import express from 'express';
+const nodemailer = require('nodemailer');
+
+
 export const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('hello router')
+    res.render('index')
 });
+router.post('/send',(req,res)=>{
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  var transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: 'youremail@a.com',
+      pass: 'yourpassword'
+    }
+  });
+    const mailOptions = {
+        from: "sender address", 
+        to: " receivers", 
+        subject: "Contact detail", // Subject line
+        html: output,
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        res.render('index', {msg:'Email has been sent'});
+        }
+    });
+
+   
+
+
+});
+
+
+
+export default router;
